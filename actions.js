c96942b14e72c3f52f8883a737ace028e131dbed
LAPTOP-3AOD70UU\maxma <magin.maxim@gmail.com>
2020-10-14 19:48:29 +0300

    ф

--- actions.js
+++ actions.js
@@ -1,163 +1,229 @@
-
-window.onload = function () {
-    $(document).ready(function () {
-        let toDoList = new Array();
-        let activeItemsList = new Array();
-        let complitedItemsList = new Array();
-
-        let inputValue;
-        let liIndex = 0;
+const form = document.querySelector("#form");
+const input = document.querySelector("#toDoListInput");
+const toDoList = document.querySelector("#list");
+const icon = document.querySelector("form").firstElementChild;
 
 
-        document.getElementById("toDoListInput").addEventListener("keydown", function (e) {
-            let keyCode = e.keyCode || e.which;
-            if (keyCode === 13) {
-                // enter pressed
-                inputValue = $("#toDoListInput").val();
-                if (inputValue != "") {
-                    toDoList.push({
-                        value: inputValue,
-                        checked: false,
-                        id: liIndex,
-                    });
-                    console.log(toDoList);
-                    e.preventDefault();
-                    $('#list').empty();
-                    if (document.querySelector(".selected").id == 'allItems') {
-                        addItems(toDoList, $("#filters li a .selected").attr('id'));
-                    }
-                    else if (document.querySelector(".selected").id == 'activeItems') {
-                        activeItemsList = toDoList.filter(item => item.checked == false);
-                        addItems(activeItemsList, $("#filters li a .selected").attr('id'));
-                    }
-                    else {
-                        complitedItemsList = toDoList.filter(item => item.checked == true);
-                        addItems(complitedItemsList, $("#filters li a .selected").attr('id'));
-                    }
-                    liIndex++;
-                    $("#toDoListInput").val(null);
-                }
-                else {
-                    e.preventDefault();
-                }
+form.addEventListener("submit", addToDo);
+toDoList.addEventListener("click", checkDivButtons);
+footer.addEventListener("click", checkFooterButtons);
+icon.addEventListener("click", manageArrowDown);
+
+footer.style["display"] = "none";
+
+// Считает количество тасков
+function countNoCompletedTasks() {
+    const noCompletedTasks = Array.from(toDoList.children).filter((element) => element.firstElementChild.className.includes("no_checked") === true);
+    return noCompletedTasks.length;
+}
+
+// Изменяет количество выполненных тасков
+function changeCountItems(count) {
+    let countItems = document.querySelector(".count_items");
+    if (countItems !== null) {
+        countItems.textContent = count === 1 ? "1 item left" : `${count} items left`;
+    }
+}
+
+// Если таск выполнен, то делает его невыполненным и наоборот
+function checkStatusTask(task) {
+    let span = task.firstElementChild;
+
+    span.classList.toggle("checked");
+    span.classList.toggle("no_checked");
+    span.nextElementSibling.classList.toggle("strikethrough");
+    span.nextElementSibling.classList.toggle("no_strikethrough");
+}
+
+// Делает все таски выполненными
+function makeCheckEverything(element) {
+    element.firstElementChild.classList.add("checked");
+    element.firstElementChild.classList.remove("no_checked");
+    element.firstElementChild.nextElementSibling.classList.add("strikethrough");
+    element.firstElementChild.nextElementSibling.classList.remove("no_strikethrough");
+}
+
+// Делает все таски выполненными
+function manageArrowDown(e) {
+    const currentButton = footer.querySelector(".choised").className;
+
+    e.target.classList.toggle("clicked");
+    e.target.classList.toggle("no_clicked");
+
+    // Если ни один из тасков не завершён
+    if (countNoCompletedTasks() === toDoList.childElementCount) {
+        Array.from(toDoList.children).forEach((element) => checkStatusTask(element));
+
+        filter(currentButton);
+        manageVisibilityClearCompleted();
+    }
+    // Если все таски завершены
+    else if (countNoCompletedTasks() === 0) {
+        Array.from(toDoList.children).forEach((element) => checkStatusTask(element));
+
+        filter(currentButton);
+        manageVisibilityClearCompleted();
+    }
+    // Если какой-то из тасков не завершён
+    else {
+        Array.from(toDoList.children).forEach((element) => makeCheckEverything(element));
+
+        filter(currentButton);
+        manageVisibilityClearCompleted();
+    }
+
+    changeCountItems(countNoCompletedTasks());
+}
+    
+// Удаляет таск
+function deleteTask(e) {
+    e.parentNode.remove();
+
+    if (toDoList.childElementCount === 0) {
+        footer.style["display"] = "none";
+        icon.className = "";
+    }
+
+    changeCountItems(countNoCompletedTasks());
+}
+
+// Управляет видимостью кнопки "Clear completed"
+function manageVisibilityClearCompleted() {
+    const buttonClearCompleted = footer.querySelector(".clearCompleted");
+    buttonClearCompleted.style["display"] = toDoList.childElementCount - countNoCompletedTasks() !== 0 ? "block" : "none"
+}
+
+// Делает таск выполненным
+function makeTaskChecked(e) {
+    const currentButton = footer.querySelector(".choised").className;
+    
+    checkStatusTask(e.target.parentNode);
+
+    changeCountItems(countNoCompletedTasks());
+
+    manageVisibilityClearCompleted();
+
+    filter(currentButton);
+
+    if (countNoCompletedTasks() === 0) {
+        icon.classList.add("clicked");
+        icon.classList.remove("no_clicked");
+    }
+    else {
+        icon.classList.add("no_clicked");
+        icon.classList.remove("clicked");
+    }
+}
+
+// Проверка кнопок complete и delete
+function checkDivButtons(e) {
+    // Если была нажата кнопка complete
+    if (e.target.className.includes("span_complete"))
+    {   
+        makeTaskChecked(e);
+    }
+    // Если была нажата кнопка delete
+    if (e.target.className.includes("span_delete"))
+    {
+        deleteTask(e.target);
+    }
+}
+
+// HTML код нового таска
+function templateTask(text) {
+    const template = `
+    <div class="new_task">
+        <span class="span_complete no_checked"></span>
+        <label class="label" >${text}</label>
+        <span class="span_delete"></span>
+    </div>`;
+
+    return template;
+}
+
+function checkFooterButtons(e) {
+    const buttonContainer = document.querySelector(".links");
+    if (e.target.tagName === "A") {
+        e.target.classList.add("choised");
+
+        // В зависимости от выбранной кнопки, остальные теряют фокус
+        Array.from(buttonContainer.children).forEach((link) => {
+            if (!link.firstElementChild.className.includes(e.target.className)) {
+                link.firstElementChild.classList.remove("choised");
             }
-        }, false);
-
-        // if ($("#filters .selected").attr(id) == "allitems") {
-        //     addItems(toDoList);
-        // }
-        // else if($("#filters .selected").attr(id) == "activeitems"){
-        //     let activeItemsList = toDoList.filter(item => item.active == false);
-        //     addItems(activeItemsList);
-        // }
-        // else{
-        //     let complitedItemsList = toDoList.filter(item => item.active == true);
-        //     addItems(complitedItemsList);
-        // }
-
-        $("#filters li a").click(function (e) {
-            e.preventDefault();
-            $("#filters li a").removeClass("selected");
-            $(this).addClass("selected");
-            $('#list').empty();
-            if (this.id == 'allItems') {
-                addItems(toDoList, this.id);
-            }
-            else if (this.id == 'activeItems') {
-                activeItemsList = toDoList.filter(item => item.checked == false);
-                addItems(activeItemsList, this.id);
-            }
-            else {
-                complitedItemsList = toDoList.filter(item => item.checked == true);
-                addItems(complitedItemsList, this.id);
-            }
-        });
-
-        function addItems(array, arrayType) {
-            let liClass = "";
-            if(arrayType == "allItems"){
-                for (let i = 0; i < array.length; ++i) {
-                    if(array[i].checked){
-                        liClass = "chosen";
-                    }
-                    else{
-                        liClass = "";
-                    }
-                    $("#list").append(`
-                        <li class="listItem ${liClass}" id="${array[i].id}">
-                            <div class="row">
-                                <div class="row">
-                                    <input type="checkbox" class="check">
-                                    <h4>${array[i].value}</h4>
-                                </div>
-                            </div>
-                            <hr>
-                        </li>
-                    `);
-                    
-                }
-                liClass = "";
-            }
-            else if (arrayType =="completedItems"){
-                liClass = "chosen";
-                for (let i = 0; i < array.length; ++i) {
-                    $("#list").append(`
-                        <li class="listItem ${liClass}" id="${array[i].id}">
-                            <div class="row">
-                                <div class="row">
-                                    <input type="checkbox" class="check" checked>
-                                    <h4>${array[i].value}</h4>
-                                </div>
-                            </div>
-                            <hr>
-                        </li>
-                    `);
-                }
-                liClass = "";
-            }
-           else{
-            for (let i = 0; i < array.length; ++i) {
-                $("#list").append(`
-                    <li class="listItem ${liClass}" id="${array[i].id}">
-                        <div class="row">
-                            <div class="row">
-                                <input type="checkbox" class="check">
-                                <h4>${array[i].value}</h4>
-                            </div>
-                        </div>
-                        <hr>
-                    </li>
-                `);
-                if(array[i].checked == true){
-                    document.querySelector('#list > li:last-child').checked = true;
-                }
-            }
-           }
-        }
-
-        $('#list').on('change', '.check', (ev) => {
-            let $target = $(ev.currentTarget);
-
-            if ($target.prop('checked') == true) {
-                $target.closest('.listItem').addClass("chosen");
-                let selectedID = $target.closest('.listItem').attr('id');
-                toDoList.find(item => item.id == selectedID).checked = true;
-            }
-            else {
-                $target.closest('.listItem').removeClass("chosen");
-                let selectedID = $target.closest('.listItem').attr('id');
-                toDoList.find(item => item.id == selectedID).checked = false;
-            }
-            console.log(ev.currentTarget.checked);
-
         })
 
-        $("#cleaner").on('click', function (e) {
-            e.preventDefault();
-            $("#list .chosen").remove();
-            toDoList = toDoList.filter(item => item.checked == false)
-            console.log(toDoList);
-        })
+        filter(e.target.className);
+    }
+}
 
+function filter(currentButton) {
+    const allTasks = Array.from(toDoList.children);
+
+    if (currentButton.includes("all")) {
+        filterALL(allTasks);
+    }
+    else if (currentButton.includes("active")) {
+        filterActive(allTasks);
+    }
+    else if (currentButton.includes("completed")) {
+        filterCompleted(allTasks);
+    }
+    else if (currentButton.includes("clearCompleted")) {
+        clearAllCompleted(allTasks);
+    }
+}
+
+// Показать все
+function filterALL(allTasks) {
+    allTasks.forEach((element) => element.style.display = "flex");
+}
+
+// Показать активные
+function filterActive(allTasks) {
+    allTasks.forEach((element) => {
+        element.style["display"] = element.firstElementChild.className.includes("no_checked") ? "flex" : "none";
     });
+}
+
+// Показать выполненные
+function filterCompleted(allTasks) {
+    allTasks.forEach((element) => {
+        element.style["display"] = element.firstElementChild.className.includes("no_checked") ? "none" : "flex";
+    });
+}
+
+// Удаляет все выполненные таски
+function clearAllCompleted(allTasks) {
+    allTasks.forEach((element) => {
+        if (element.firstElementChild.className.includes("no_checked") === false) {
+            deleteTask(element.firstElementChild)
+        }
+    })
+}
+
+// Добавляет новый таск, футер и иконку
+function renderTask (template) {
+    toDoList.innerHTML += template;
+
+    const currentButton = footer.querySelector(".choised").className;
+    const allTasks = Array.from(toDoList.children);
+
+    if (currentButton.includes("completed")) {
+        filterCompleted(allTasks);
+    }
+
+    icon.className = "icon no_clicked";
+    footer.style["display"] = "block";
+    changeCountItems(countNoCompletedTasks());
+}
+
+// Проверяет input на наличие текста и добавляет таск
+function addToDo(e) {
+    e.preventDefault();
+
+    if (input.value !== "") {
+        renderTask(templateTask(input.value));
+        input.value = "";
+    }
 }
