3f5e2da57b96e8d2c1098946ddc35b3c3f23aa65
LAPTOP-3AOD70UU\maxma <magin.maxim@gmail.com>
2020-10-15 00:43:12 +0300

    action.js refactoring

--- actions.js
+++ actions.js
@@ -2,8 +2,6 @@
 window.onload = function () {
     $(document).ready(function () {
         let toDoList = new Array();
-        let activeItemsList = new Array();
-        let complitedItemsList = new Array();
 
         let inputValue;
         let liIndex = 0;
@@ -14,26 +12,26 @@ window.onload = function () {
             if (keyCode === 13) {
                 // enter pressed
                 inputValue = $("#toDoListInput").val();
+
                 if (inputValue != "") {
                     toDoList.push({
                         value: inputValue,
                         checked: false,
                         id: liIndex,
                     });
-                    console.log(toDoList);
                     e.preventDefault();
+
                     $('#list').empty();
-                    if (document.querySelector(".selected").id == 'allItems') {
-                        addItems(toDoList, document.querySelector(".selected").id);
+
+                    let itemsList = toDoList;
+                    if (document.querySelector(".selected").id == 'activeItems') {
+                        itemsList = toDoList.filter(item => item.checked == false);
                     }
-                    else if (document.querySelector(".selected").id == 'activeItems') {
-                        activeItemsList = toDoList.filter(item => item.checked == false);
-                        addItems(activeItemsList, document.querySelector(".selected").id);
+                    else if (document.querySelector(".selected").id == 'completedItems') {
+                        itemsList = toDoList.filter(item => item.checked == true);
                     }
-                    else {
-                        complitedItemsList = toDoList.filter(item => item.checked == true);
-                        addItems(complitedItemsList, document.querySelector(".selected").id);
-                    }
+                    displayItems(itemsList, document.querySelector(".selected").id);
+
                     liIndex++;
                     $("#toDoListInput").val(null);
                     document.querySelector(".icon").style["visibility"] = "visible";
@@ -48,38 +46,55 @@ window.onload = function () {
 
         $("#filters li a").click(function (e) {
             e.preventDefault();
+
             if (this.id != "cleaner") {
                 $("#filters li a").removeClass("selected");
                 $(this).addClass("selected");
+
                 $('#list').empty();
-                if (this.id == 'allItems') {
-                    addItems(toDoList, this.id);
+
+                let itemsList = toDoList;
+                if (document.querySelector(".selected").id == 'activeItems') {
+                    itemsList = toDoList.filter(item => item.checked == false);
                 }
-                else if (this.id == 'activeItems') {
-                    activeItemsList = toDoList.filter(item => item.checked == false);
-                    addItems(activeItemsList, this.id);
+                else if (document.querySelector(".selected").id == 'completedItems') {
+                    itemsList = toDoList.filter(item => item.checked == true);
                 }
-                else {
-                    complitedItemsList = toDoList.filter(item => item.checked == true);
-                    addItems(complitedItemsList, this.id);
-                }
+                displayItems(itemsList, document.querySelector(".selected").id);
             }
             else {
                 e.preventDefault();
+
                 $("#list .chosen").remove();
+
                 toDoList = toDoList.filter(item => item.checked == false);
-                console.log(toDoList);
+
                 if (toDoList.length == 0) {
                     document.querySelector(".icon").style["visibility"] = "hidden";
                 }
-                if(toDoList.filter(item => item.checked == true).length == 0){
+                else if (toDoList.filter(item => item.checked == true).length == 0) {
                     document.querySelector("#cleaner").style["visibility"] = "hidden";
                 }
             }
 
         });
 
-        function addItems(array, arrayType) {
+        function addItem(liClass, item){
+            $("#list").append(`
+                        <li class="listItem ${liClass}" id="${item.id}">
+                            <div class="row">
+                                <div class="row">
+                                    <input type="checkbox" class="check" id="${item.id}check">
+                                    <label for="${item.id}check"></label>
+                                    <h4>${item.value}</h4>
+                                </div>
+                            </div>
+                            <hr>
+                        </li>
+                    `);
+        }
+
+        function displayItems(array, arrayType) {
             let liClass = "";
             if (arrayType == "allItems") {
                 for (let i = 0; i < array.length; ++i) {
@@ -89,18 +104,7 @@ window.onload = function () {
                     else {
                         liClass = "";
                     }
-                    $("#list").append(`
-                        <li class="listItem ${liClass}" id="${array[i].id}">
-                            <div class="row">
-                                <div class="row">
-                                    <input type="checkbox" class="check" id="${array[i].id}check">
-                                    <label for="${array[i].id}check"></label>
-                                    <h4>${array[i].value}</h4>
-                                </div>
-                            </div>
-                            <hr>
-                        </li>
-                    `);
+                    addItem(liClass, array[i])
 
                 }
                 document.querySelectorAll(".chosen .check").forEach((item) => item.checked = true);
@@ -110,36 +114,14 @@ window.onload = function () {
             else if (arrayType == "completedItems") {
                 liClass = "chosen";
                 for (let i = 0; i < array.length; ++i) {
-                    $("#list").append(`
-                        <li class="listItem ${liClass}" id="${array[i].id}">
-                            <div class="row">
-                                <div class="row">
-                                    <input type="checkbox" class="check" id="${array[i].id}check">
-                                    <label for="${array[i].id}check"></label>
-                                    <h4>${array[i].value}</h4>
-                                </div>
-                            </div>
-                            <hr>
-                        </li>
-                    `);
+                    addItem(liClass, array[i])
                 }
                 document.querySelectorAll(".chosen .check").forEach((item) => item.checked = true);
                 liClass = "";
             }
             else {
                 for (let i = 0; i < array.length; ++i) {
-                    $("#list").append(`
-                    <li class="listItem ${liClass}" id="${array[i].id}">
-                        <div class="row">
-                            <div class="row">
-                                <input type="checkbox" class="check" id="${array[i].id}check">
-                                <label for="${array[i].id}check"></label>
-                                <h4>${array[i].value}</h4>
-                            </div>
-                        </div>
-                        <hr>
-                    </li>
-                `);
+                    addItem(liClass, array[i])
                     if (array[i].checked == true) {
                         document.querySelector('#list > li:last-child').checked = true;
                     }
@@ -161,12 +143,11 @@ window.onload = function () {
                 $target.closest('.listItem').removeClass("chosen");
                 let selectedID = $target.closest('.listItem').attr('id');
                 toDoList.find(item => item.id == selectedID).checked = false;
-                if(toDoList.filter(item => item.checked == true).length == 0){
+                
+                if (toDoList.filter(item => item.checked == true).length == 0) {
                     document.querySelector("#cleaner").style["visibility"] = "hidden";
                 }
             }
-            console.log(ev.currentTarget.checked);
-
         })
 
         $(".icon").click(function (e) {
@@ -175,7 +156,7 @@ window.onload = function () {
                 item.checked = true;
             })
             $("#list").empty();
-            addItems(toDoList, document.querySelector(".selected").id)
+            displayItems(toDoList, document.querySelector(".selected").id)
             document.querySelector("#cleaner").style["visibility"] = "visible";
         })
 
